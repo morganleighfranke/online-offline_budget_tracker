0838f5169c8e2aa55824d71fc99c00c80070b1f8
Morgan Franke <morganleighfranke@gmail.com>
2021-06-01 16:28:22 -0400

    heroku

--- Develop/public/service-worker.js
+++ Develop/public/service-worker.js
@@ -1,9 +1,11 @@
 const FILES_TO_CACHE = [
     '/',
-    '/index.html',
+    '/index.js',
     '/manifest.json',
     '/style.css',
-    'https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css',
+    '/indexedDB.js',
+    '/icons/icon-192x192.png',
+    '/icons/icon-512x512.png',
   ];
   
   const PRECACHE = 'precache-v1';
